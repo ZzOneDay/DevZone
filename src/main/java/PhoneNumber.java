8a70aa162c2e64d6188abd6a4c319f8eac416867
ZzOneDay <azminseo@gmail.com>
2020-10-01 04:00:35 +0300

    Initial commit

---
+++ src/main/java/PhoneNumber.java
@@ -0,0 +1,16 @@
+/**
+ * Phone number model.
+ */
+public interface PhoneNumber {
+    /**
+     * Get phone number value.
+     * @return number value
+     */
+    String getPhoneNumber();
+
+    /**
+     * Set phone number value.
+     * @param phoneNumber number value
+     */
+    void setPhoneNumber(String phoneNumber);
+}
